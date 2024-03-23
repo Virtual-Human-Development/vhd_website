ed6b35452b41ac50baa0a61ec4b69802fde03e8e
stenhold <doctorduality@gmail.com>
2024-03-23 14:30:38 -0700

    just in time for novus

--- pages/about.tsx
+++ pages/about.tsx
@@ -33,27 +33,81 @@ export default function Home() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
                     <div>
                         <h2 className="text-lg font-bold" style={{ color: 'var(--text-color)' }}>
-                            Join our community and make a difference.
+                            A consortium of academics in mathematical biology.
                         </h2>
                         <p className="text-md mt-4" style={{ color: 'var(--secondary-text-color)' }}>
-                            Join our consortium to make a positive impact in your industry. Connect with like-minded professionals and stay up-to-date on industry developments.
+                            Working together toward increasingly integrated, capable simulations of the entire human developmental process.
                         </p>
                     </div>
-                    <Image src="/vhd_banner_1.jpg" alt="Placeholder" width={400} height={400} className="rounded-lg shadow-lg" />
+                    <Image src="/consortium.jpg" alt="Placeholder" width={400} height={400} className="rounded-lg shadow-lg" />
                 </div>
             </main>
 
-            <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+                    {/* Our Mission Column */}
                     <div>
                         <h2 className="text-lg font-bold" style={{ color: 'var(--text-color)' }}>
-                            The next generation of human developmental models.
+                            Our Mission
                         </h2>
                         <p className="text-md mt-4" style={{ color: 'var(--secondary-text-color)' }}>
-                            Leveraging a high-impact community with decades of modeling experience.
+                            The Virtual Human Development Consortium brings together a global community of scholars and policy makers around the central goal:
+                            <span className="pl-1" style={{ color: 'var(--text-color)' }}>creating a computer-based simulator of human embryonic development.</span>
+                            <br /><br />
+                            Our mission is to produce a state of the art simulator with the ability to predict efficiently and effectively the outcome of cellular tissue and potential regenerative therapies.
+                            <br /><br /><br />
+                            The simulator will be a powerful tool that will:
+                            <br />
+                            <br />
+                            <ul className="pl-6 list-disc">
+                                <li>Provide<span className="pl-1" style={{ color: 'var(--text-color)' }}>a window into the formation of the human body </span>, catalyzing new hypotheses for the developmental biology field to push the boundaries outside of what we know about our origins; and</li>
+                                <li>Serve as a <span className="pl-1" style={{ color: 'var(--text-color)' }}>platform for rational cellular design</span>, allowing us to engineer biological substrates for regenerative medicine. The model can also be adapted for personalized medicine approaches.</li>
+                            </ul>
+                            <br />
+                            <br />
+                            Such a simulator will be the key tool to catalyze rapid rounds of computational modeling in the biotechnology sector, fueling the design-build-test cycle for lab-grown cells and tissues.
+                            It will serve as a tool for simulating engineered developmental systems, leading to robust bioprocesses for manufacturing designer cells, tissues, and organs on-demand.
                         </p>
                     </div>
-                    <Image src="/VHD_banner_2.jpg" alt="Placeholder" width={400} height={400} className="rounded-lg shadow-lg" />
+
+                    {/* Our Approach Column */}
+                    <div>
+                        <h2 className="text-lg font-bold" style={{ color: 'var(--text-color)' }}>
+                            Our Approach
+                        </h2>
+                        <p className="text-md mt-4" style={{ color: 'var(--secondary-text-color)' }}>
+                            Virtual Human Development will serve as an
+                            <span className="pl-1 pr-1" style={{ color: 'var(--text-color)' }}>
+                                intellectual sandbox for international groups
+                            </span>to understand and build biology together,
+                            guided by our common mission. We will collectively develop and integrate our knowledge into a human embryo simulation platform in
+                            line with the ethics, legal and social implications of embryo simulation. To enable this, we have three core values:
+                            <br />
+                            <br />
+                            <span className="pr-2" style={{ color: 'var(--text-color)' }}>
+                                Collaboration:
+                            </span>
+                            We will foster an environment of scientific integrity, ensuring we apply best practices and know-how to generate reliable
+                            experimental datasets and modeling frameworks.
+                            <br />
+                            <br />
+                            <span className="pr-2" style={{ color: 'var(--text-color)' }}>
+                                Integrity:
+                            </span>
+                            We will foster an environment of scientific integrity, ensuring we apply best practices and know-how to generate reliable
+                            experimental datasets and modeling frameworks.
+                            <br />
+                            <br />
+                            <span className="pr-2" style={{ color: 'var(--text-color)' }}>
+                                Posterity:
+                            </span>
+                            We believe in the importance of training and guiding the next generation of innovators who will build on and expand our
+                            consortium&#39;s progress in developing a human embryo simulator. To do this, we will embed trainees in our governance structure and
+                            dedicate resources to ensure they are well-supported and trained in professional and technical skills.
+                        </p>
+                    </div>
+
                 </div>
             </main>
 
